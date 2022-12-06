79cb66040965299a07a1e2af431a4c8db588c0cf
Kwon20 <96705867+Kwon20@users.noreply.github.com>
2022-12-06 10:47:26 +0900

    Update DungeonGenarationData.cs

--- Random/DungeonGenarationData.cs
+++ Random/DungeonGenarationData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="DungeonGenerationData.asset",menuName ="DungeonGenertationData/Dungeon Data")]
 public class DungeonGenarationData : ScriptableObject
 {
-    public int numberOfCrawlers;
-    public int iterationMin;
-    public int iterationMax;
+    public int numberOfCrawlers;    //크롤러의 갯수
+    public int iterationMin;        //방의 최소갯수
+    public int iterationMax;        //방의 최대갯수
 }
